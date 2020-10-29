732be4ce3c0a3f0bbcefd042bbf1a838dc7defd9
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-10-29 11:12:00 +0100

    fix: Flatcar support
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -2,6 +2,8 @@
 set -e
 
 BINDIR="/opt/bin"
+PYPY_VERSION=7.3.2
+PYPI_URL="https://downloads.python.org/pypy/pypy3.6-v${PYPY_VERSION}-linux64.tar.bz2"
 
 mkdir -p $BINDIR
 
@@ -11,21 +13,12 @@ if [[ -e $BINDIR/.bootstrapped ]]; then
   exit 0
 fi
 
-PYPY_VERSION=7.0.0
+TAR_FILE=pyp.tar.bz2
+wget -O "${TAR_FILE}" "${PYPI_URL}"
+tar -xjf "${TAR_FILE}" && rm "${TAR_FILE}"
+mv -n "pypy3.6-v${PYPY_VERSION}-linux64" pypy3
 
-wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 |tar -xjf -
-mv -n pypy3.5-$PYPY_VERSION-linux_x86_64-portable pypy
-
-## library fixup
-mkdir -p pypy/lib
-ln -snf /lib64/libncurses.so.6.1 $BINDIR/pypy/lib/libtinfo.so.5
-
-cat > $BINDIR/python <<EOF
-#!/bin/bash
-LD_LIBRARY_PATH=$BINDIR/pypy/lib:$LD_LIBRARY_PATH exec $BINDIR/pypy/bin/pypy3 "\$@"
-EOF
-
-chmod +x $BINDIR/python
+ln -s ./pypy3/bin/pypy3 python
 $BINDIR/python --version
 
 touch $BINDIR/.bootstrapped
